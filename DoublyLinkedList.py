7e7ce264eb95ced1e8d691e711e74c11e18f5faa
Patrick <patrick.smith@uconn.edu>
2017-10-02 17:47:35 -0400

    Refactoring
    added _addbetween, _remove, and __iadd__ methods

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -7,23 +7,52 @@ class DoublyLinkedList:
         self._tail = None
         self._length = 0
 
-    def addfirst(self, item):
-        if len(self) == 0:
-            self._head = self._tail = ListNode(item, None, None)
-        else:
-            newnode = ListNode(item, None, self._head)
-            self._head.prev = newnode
-            self._head = newnode
-        self._length += 1
-
-    def addlast(self, item):
-        if len(self) == 0:
-            self._head = self._tail = ListNode(item, None, None)
-        else:
-            newnode = ListNode(item, self._tail, None)
-            self._tail.link = newnode
-            self._tail = newnode
-        self._length += 1
-
     def __len__(self):
         return self._length
+
+    def __iadd__(self, other):  # overrides += operator
+        if other._head is None:
+            return
+        if self._head is None:
+            self._head = other._head
+        else:
+            self._tail.link = other._head
+            other._head.prev = self._tail
+        self._tail = other._tail
+        self._length = self._length + other._length
+
+        other.__init__()
+        return self
+
+    def _addbetween(self, item, before, after):
+        node = ListNode(item, before, after)
+        if after is self._head:
+            self._head = node
+        if before is self._tail:
+            self._tail = node
+        self._length += 1
+
+    def addfirst(self, item):
+        self._addbetween(item, None, self._head)
+
+    def addlast(self, item):
+        self._addbetween(item, self._tail, None)
+
+    def _remove(self, node):
+        before, after = node.prev, node.link
+        if node is self._head:
+            self._head = after
+        else:
+            before.link = after
+        if node is self._tail:
+            self._tail = before
+        else:
+            after.prev = before
+        self._length -= 1
+        return node.data
+
+    def removefirst(self):
+        return self._remove(self._head)
+
+    def removelast(self):
+        return self._remove(self._tail)
